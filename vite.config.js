9b5b8337ae5becc78bb071f95e0d5f0d5144d6b6
Nathan81824 <Nathanmoses.u@gmail.com>
2026-08-25 19:22:05 -0700

    publihed portfolio again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/",
+  base: "/Nathan-Frontend-Developer/",
 
   build: {
     outDir: "dist",
